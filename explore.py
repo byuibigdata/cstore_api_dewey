d2f055acd543d8321af3858ca52902d19847e452
J. Hathaway <hathawayj@byui.edu>
2024-05-22 14:33:05 -0600

    good example moved to Databricks under dewey_load

--- explore.py
+++ explore.py
@@ -2,91 +2,137 @@
 import requests
 import polars as pl
 import os
+from io import StringIO
+from itertools import chain
 from pprint import pprint
-from dotenv import load_dotenv
-load_dotenv()
-API_KEY = os.environ.get("PDI_API_KEY")
+from dotenv import load_dotenv, find_dotenv
+load_dotenv("../../.env")
+first_portion = "paoaaHN2."
+API_KEY = first_portion + os.environ.get("PDI_API_KEY")
 
-ep_names = [
-    "transaction_items",
-    "daily_transaction_aggregations",
-    "store_information",
-    "credit_card_ayments",
-    "discounts",
-    "transaction_sets",
-    "shopper_id",
-    "gtin_master_file",
-    "payments",
-    "store_status",
-    "store_information"
+# documentation https://github.com/amplifydata/amplifydata-public/blob/main/README.md
 
-]
-
+aws_names = ["pdi_masterGTIN", "pdi_storestatus", "pdi_stores"]
+end_names = ["transaction_items","discounts","transactions_daily_agg", "transaction_sets", "payments"]
+end_names_nodate = ["shopper_id"]
 endpoints = [
-    "https://app.deweydata.io/external-api/v3/products/5d474ecc-389c-4057-b105-fd5d079e4a5c/files",
-    "https://app.deweydata.io/external-api/v3/products/81f00a32-da42-4e45-b072-4a2db6e05a32/files",
-    "https://app.deweydata.io/external-api/v3/products/e134b3ff-671c-4566-8e41-17ab64395b0b/files",
-    "https://app.deweydata.io/external-api/v3/products/5ad40e0b-afd8-4764-a7f4-d23502511566/files",
-    "https://app.deweydata.io/external-api/v3/products/49433fe0-c25e-4348-a8a9-1dd57dfcce4a/files",
-    "https://app.deweydata.io/external-api/v3/products/08b48f19-9cbf-441c-abfc-3529c784702f/files",
-    "https://app.deweydata.io/external-api/v3/products/b4229432-c324-481d-a416-37386a0b0a8d/files",
-    "https://app.deweydata.io/external-api/v3/products/dfc72515-51f0-4363-842f-a9b3b38cdfd0/files",
-    "https://app.deweydata.io/external-api/v3/products/f4048bae-bd39-416e-a501-a620e66b285f/files",
-    "https://app.deweydata.io/external-api/v3/products/0dbf3f6b-1e5f-4615-b65a-777274f48f50/files",
-    "https://app.deweydata.io/external-api/v3/products/e134b3ff-671c-4566-8e41-17ab64395b0b/files"
+    "https://app.deweydata.io/external-api/v3/products/935801cb-043c-405e-b2de-24a47d36620d/files",
+    "https://app.deweydata.io/external-api/v3/products/9f1198d7-f3c8-4351-8557-ab2de0c580c1/files",
+    "https://app.deweydata.io/external-api/v3/products/4e393e1c-bde7-4c5a-a6ab-207abeffd733/files",
+    "https://app.deweydata.io/external-api/v3/products/89c21467-16d0-4302-87cf-4e59a79b7fce/files",
+    "https://app.deweydata.io/external-api/v3/products/97595252-5da9-4cfe-b063-7794159d997c/files",
 ]
-PRODUCT_API_PATH = endpoints[1]
+endpoints_nodate = ["https://app.deweydata.io/external-api/v3/products/3291566f-9454-49e3-a4b3-3b3a557f1bee/files"]
+metadata_path = "/metadata"
+PRODUCT_API_PATH = endpoints[0]
+start_date = '2021-01-01'
+end_date = '2024-03-01'
 
 # %%
-# Metadata Information
-results = requests.get(url=PRODUCT_API_PATH+"/metadata",
-                       headers={
-                        "X-API-KEY": API_KEY,
-                        'accept': 'application/json'
-                       })
-pprint(results.json())
-# {'max_partition_key': '2023-12-01',
-#  'min_partition_key': '2019-07-01',
-#  'partition_aggregation': 'MONTH',
-#  'partition_column': 'DATE_TIME',
-#  'total_files': 13524,
-#  'total_size': 2830460926441}
+en_loop = 0
+for PRODUCT_API_PATH in endpoints:
+  results = requests\
+    .get(url=PRODUCT_API_PATH,
+      params={'page': 1, # only getting 1st page of results
+        'partition_key_after': start_date, # set date value here
+        'partition_key_before': end_date}, # set date value here
+      headers={'X-API-KEY': API_KEY, 'accept': 'application/json'})\
+    .json()
+
+  total_pages = results['total_pages']
+  print("Total pages for " + PRODUCT_API_PATH)
+  print(total_pages)
+
+  list_links = [requests.get(
+      url=PRODUCT_API_PATH,
+      params={'page': i, 'partition_key_after': start_date, 'partition_key_before': end_date},
+      headers={'X-API-KEY': API_KEY, 'accept': 'application/json'}).json() for i in range(1, total_pages + 1)]
+
+  first = 0
+  for i in list_links:
+    if first == 0:
+      dat = pl.DataFrame(i)
+    if first >= 0:
+      dat = pl.concat([dat, pl.DataFrame(i)], rechunk=True)
+  print(first)
+  first += 1      
+
+  if en_loop == 0:
+     out = dat.with_columns(pl.lit(PRODUCT_API_PATH).alias("api_path"))
+  if en_loop >= 0:
+     out = pl.concat([out, dat.with_columns(pl.lit(PRODUCT_API_PATH).alias("api_path"))])
+  print("Endpoint Loop")
+  print(en_loop)
+  en_loop += 1
+
+dat_links_datechunk = out.unnest("download_links")
+
 # %%
-results = requests.get(url=PRODUCT_API_PATH,
-                       params={'page': 1, # only getting 1st page of results
-                               'partition_key_after': '1900-01-01', # set date value here
-                               'partition_key_before': '2099-01-01'}, # set date value here
-                       headers={'X-API-KEY': API_KEY,
-                                'accept': 'application/json'
-                               })
-pprint(results.json())
+en_loop = 0
+for PRODUCT_API_PATH in endpoints_nodate:
+  results = requests\
+    .get(url=PRODUCT_API_PATH,
+      params={'page': 1}, # only getting 1st page of results
+      headers={'X-API-KEY': API_KEY, 'accept': 'application/json'})\
+    .json()
+
+  total_pages = results['total_pages']
+  print("Total pages for " + PRODUCT_API_PATH)
+  print(total_pages)
+
+  list_links = [requests.get(
+      url=PRODUCT_API_PATH,
+      params={'page': i},
+      headers={'X-API-KEY': API_KEY, 'accept': 'application/json'}).json() for i in range(1, total_pages + 1)]
+
+  first = 0
+  for i in list_links:
+    if first == 0:
+      dat = pl.DataFrame(i)
+    if first >= 0:
+      dat = pl.concat([dat, pl.DataFrame(i)], rechunk=True)
+  print(first)
+  first += 1      
+
+  if en_loop == 0:
+     out = dat.with_columns(pl.lit(PRODUCT_API_PATH).alias("api_path"))
+  if en_loop >= 0:
+     out = pl.concat([out, dat.with_columns(pl.lit(PRODUCT_API_PATH).alias("api_path"))])
+  print("Endpoint Loop")
+  print(en_loop)
+  en_loop += 1
+
+dat_links = out.unnest("download_links")
+
+
 # %%
-page = 1
-download_count = 0
-while True:
-    # get results from API endpoint, using API key for authentication
-    results = requests.get(url=PRODUCT_API_PATH,
-                           params={'page': page,
-                                   'partition_key_after': '2023-10-01',   # set date value here
-                                   'partition_key_before': '2023-12-31'}, # set date value here
-                           headers={'X-API-KEY': API_KEY,
-                                    'accept': 'application/json'
-                                   })
-    response_json = results.json()
+download_links = pl.concat([dat_links_datechunk, dat_links])\
+  .join(pl.DataFrame({"folder_name":end_names + end_names_nodate,
+                      "api_path":endpoints + endpoints_nodate}), how="left", on="api_path")\
+  .with_columns(pl.concat_str("folder_name", pl.lit("/"),"file_name").alias("write"))\
+  .with_columns(pl.struct(["link", "write"]).alias("run"))
+
+
+
+
+
+# %%
+dlinks = download_links.group_by("api_path")\
+  .agg(
+    pl.col("link").first(),
+    pl.col("run").first())
+
+urls = dlinks.select("link").to_series().to_list()
+names = dlinks.select("run").to_series().to_list()
+
+
+# %%
+def download_file(url, name):
+  data = requests.get(url)
+  with open(name, 'wb') as file:
+    file.write(data.content)
+    return(name)
     
-        # for each result page, loop through download links and save to your computer
-    for link_data in response_json['download_links']:
-        print(f"Downloading file {link_data['file_name']}...")
-        data = requests.get(link_data['link'])
-        with open(link_data['file_name'], 'wb') as file:
-            file.write(data.content)
-        download_count += 1
-
-    # only continue if there are more result pages to process
-    total_pages = response_json['total_pages']
-    if page >= total_pages:
-        break
-    page += 1
-
-print(f"Successfully downloaded {download_count} files.")
+# %%
+dlinks.with_columns(pl.col("run").map_elements(lambda cols: download_file(cols["link"], cols["write"])))
 # %%
